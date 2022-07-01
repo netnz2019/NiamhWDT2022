d8b31a7979535907c0ec004d3839c869550e63a5
NiamhWebb <18webbn@mariancollege.school.nz>
2022-07-01 13:29:39 +1200

    read book file

--- LibraryPro/src/main/java/libproject/librarypro/LibraryPro.java
+++ LibraryPro/src/main/java/libproject/librarypro/LibraryPro.java
@@ -9,6 +9,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.io.*;
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 /**
@@ -21,13 +25,16 @@ public class LibraryPro {
         Map<Long, Integer> bookMap = new HashMap<Long, Integer>();
         Map<String, String> personMap = new HashMap<String, String>();
         ArrayList<String> person = new ArrayList<String>();
-        //file
-        String fileName = "data.txt";
-        ArrayList<Person> fileData = new ArrayList<Person>();
-        readFile(fileName, fileData);
-        showList(fileData);
-       
-        
+        //person file
+        String fileNamePerson = "person.txt";
+        ArrayList<Person> fileDataPerson = new ArrayList<Person>();
+        readFilePerson(fileNamePerson, fileDataPerson);
+        showListPerson(fileDataPerson);
+       //book file
+        String fileNameBook = "book.txt";
+        ArrayList<Book> fileDataBook = new ArrayList<Book>();
+        readFileBook(fileNameBook, fileDataBook);
+        showListBook(fileDataBook);
         //People
         Person Person1 = new Person("Niamh", "Webb", 1);
         Person Person2 = new Person("Layla", "Spector", 2);
@@ -37,47 +44,59 @@ public class LibraryPro {
         Book book2 = new Book("She Gets the Girl", "Alyson Derrick and Rachael Lippincott", 9781398502635L, 2);
         Book book3 = new Book("Heartstopper Volume One", "Alice Oseman", 9781338617436L, 3);
         
-        // add people to arraylist
-        person.add(Person1.getFirstName()+" " + Person1.getLastName());
-        System.out.println(person);
-        bookMap.put(book1.getISBN(), Person1.getPersonIDnum());
-        personMap.put(Person1.getFirstName()+" " + Person1.getLastName(), book1.getBookName());
-        System.out.println(bookMap);
-        System.out.println(personMap);
-        
-        System.out.println("First name: " + Person1.getFirstName());
-        System.out.println("Last name: " + Person1.getLastName());
-        System.out.println("ID number: " + Person1.getPersonIDnum());
-        System.out.println("Book Name: " + book1.getBookName());
-        System.out.println("Author: " + book1.getAuthorName());
-        System.out.println("ISBN: " + book1.getISBN());
-        System.out.println("");
-        
-        System.out.println("First name: " + Person2.getFirstName());
-        System.out.println("Last name: " + Person2.getLastName());
-        System.out.println("ID number: " + Person2.getPersonIDnum());
-        System.out.println("Book Name: " + book2.getBookName());
-        System.out.println("Author: " + book2.getAuthorName());
-        System.out.println("ISBN: " + book2.getISBN());
-        System.out.println("");
-        
-        System.out.println("First name: " + Person3.getFirstName());
-        System.out.println("Last name: " + Person3.getLastName());
-        System.out.println("ID number: " + Person3.getPersonIDnum());
-        System.out.println("Book Name: " + book3.getBookName());
-        System.out.println("Author: " + book3.getAuthorName());
-        System.out.println("ISBN: " + book3.getISBN());
+//        // add people to arraylist
+//        person.add(Person1.getFirstName()+" " + Person1.getLastName());
+//        System.out.println(person);
+//        bookMap.put(book1.getISBN(), Person1.getPersonIDnum());
+//        personMap.put(Person1.getFirstName()+" " + Person1.getLastName(), book1.getBookName());
+//        System.out.println(bookMap);
+//        System.out.println(personMap);
+//        
+//        System.out.println("First name: " + Person1.getFirstName());
+//        System.out.println("Last name: " + Person1.getLastName());
+//        System.out.println("ID number: " + Person1.getPersonIDnum());
+//        System.out.println("Book Name: " + book1.getBookName());
+//        System.out.println("Author: " + book1.getAuthorName());
+//        System.out.println("ISBN: " + book1.getISBN());
+//        System.out.println("");
+//        
+//        System.out.println("First name: " + Person2.getFirstName());
+//        System.out.println("Last name: " + Person2.getLastName());
+//        System.out.println("ID number: " + Person2.getPersonIDnum());
+//        System.out.println("Book Name: " + book2.getBookName());
+//        System.out.println("Author: " + book2.getAuthorName());
+//        System.out.println("ISBN: " + book2.getISBN());
+//        System.out.println("");
+//        
+//        System.out.println("First name: " + Person3.getFirstName());
+//        System.out.println("Last name: " + Person3.getLastName());
+//        System.out.println("ID number: " + Person3.getPersonIDnum());
+//        System.out.println("Book Name: " + book3.getBookName());
+//        System.out.println("Author: " + book3.getAuthorName());
+//        System.out.println("ISBN: " + book3.getISBN());
     }
-  
-    public static void readFile (String fileName, ArrayList<Person> fileData){
-        File file = new File(fileName);
+    //A method that gets the string input so that can store than answer and ask question in same line
+    public static String readString(String prompt){
+        System.out.println(prompt);
+        java.util.Scanner keyboard = new java.util.Scanner(System.in);   
+        return keyboard.nextLine();
+    }
+    //method getting the int input
+    public static int readInt(String prompt){   
+    System.out.println(prompt);
+    java.util.Scanner keyboard = new java.util.Scanner(System.in);   
+    return keyboard.nextInt(); 
+    }
+  //Takes the array list and person txt file and reads the data and puts it in the array
+    public static void readFilePerson (String fileNamePerson, ArrayList<Person> fileDataPerson){
+        File file = new File(fileNamePerson);
         try{
             Scanner fileScan = new Scanner(file);
             while(fileScan.hasNextLine()){
                 String[] eachItem = fileScan.nextLine().split(",");
                 try{
                     int idNum = Integer.parseInt(eachItem[2].trim());
-                    fileData.add(new Person(eachItem[0], eachItem[1], idNum));
+                    fileDataPerson.add(new Person(eachItem[0], eachItem[1], idNum));
                 }
                 catch(NumberFormatException e){
                     System.out.println(" ignoring this line - couldn't parse ");
@@ -89,10 +108,30 @@ public class LibraryPro {
             System.exit(1);
         }
     }
-
-    public static void showList(ArrayList<Person> fileData){
+    public static void readFileBook (String fileNameBook, ArrayList<Book> fileDataBook){
+        File file = new File(fileNameBook);
+        try{
+            Scanner fileScan = new Scanner(file);
+            while(fileScan.hasNextLine()){
+                String[] eachItem = fileScan.nextLine().split(",");
+                try{
+                    int idNum = Integer.parseInt(eachItem[2].trim());
+                    fileDataBook.add(new Person(eachItem[0], eachItem[1], idNum));
+                }
+                catch(NumberFormatException e){
+                    System.out.println(" ignoring this line - couldn't parse ");
+                }
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println(" File not found ");
+            System.exit(1);
+        }
+    }
+//prints out the details of the txt file from the array list
+    public static void showListPerson(ArrayList<Person> fileDataPerson){
         System.out.println("LIST of details");
-       for(Person person:fileData){
+       for(Person person:fileDataPerson){
            System.out.println(person.getFirstName());
            System.out.println(person.getLastName());
            System.out.println(person.getPersonIDnum());
