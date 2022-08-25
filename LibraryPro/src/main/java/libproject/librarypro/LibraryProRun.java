cfa62df408a2b567c256734c612bec1a4a49fcff
NiamhWebb <18webbn@mariancollege.school.nz>
2022-08-26 11:15:23 +1200

    Create LibraryProRun.java

---
+++ LibraryPro/src/main/java/libproject/librarypro/LibraryProRun.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libproject.librarypro;
+
+/**
+ *
+ * @author niamh
+ */
+public class LibraryProRun {
+    public static void main( String args[] ){
+        javax.swing.JFrame window = new javax.swing.JFrame("LibraryPro");
+        window.getContentPane().add(new LibraryProGUI());
+        window.setVisible(true);
+        window.pack();
+        window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    }
+}
